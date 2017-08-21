314fd80561babe9067dccbe4b1938bdb16322d3e
Digaum Spinetti <mrspinetti@gmail.com>
2017-08-21 15:41:45 -0300

    Criação do git Tower Defense

---
+++ Assets/Scripts/Jogo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogo : MonoBehaviour {
+
+    [SerializeField] private GameObject torrePrefab;
+
+    [SerializeField] private GameObject gameOver;
+
+    [SerializeField] private Jogador jogador;
+
+
+	// Use this for initialization
+	void Start () {
+
+        gameOver.SetActive(false);
+		
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (JogoAcabou())
+        {
+            gameOver.SetActive(true);
+        }else
+        {
+            if (ClicouComBotaoPrimario())
+            {
+                ControiTorre();
+            }
+        }
+
+
+		
+	}
+
+    private bool JogoAcabou()
+    {
+        return !jogador.EstaVivo();
+    }
+
+    public void RecomecaJogo()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    private bool ClicouComBotaoPrimario()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    private void ControiTorre()
+    {
+        Vector3 posicaoDoClique = Input.mousePosition;
+        RaycastHit elementoAtingidoPeloRaio = DisparaRaioDaCameraAteUmPonto(posicaoDoClique);
+
+        if (elementoAtingidoPeloRaio.collider != null)
+        {
+            Vector3 posicaoDoElemento = elementoAtingidoPeloRaio.point;
+            Instantiate(torrePrefab, posicaoDoElemento, Quaternion.identity);
+        }
+    }
+
+    private RaycastHit DisparaRaioDaCameraAteUmPonto(Vector3 ponto)
+    {
+        Ray raio = Camera.main.ScreenPointToRay(ponto);
+        RaycastHit elementoAtingidoPeloRaio;
+        float comprimentoMaximoDoRaio = 100.0f;
+
+        Physics.Raycast(raio, out elementoAtingidoPeloRaio, comprimentoMaximoDoRaio);
+
+        return elementoAtingidoPeloRaio;
+    }
+}
---
+++ Assets/Scripts/MostradorDeVidas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostradorDeVidas : MonoBehaviour {
+
+    private Text campoTexto;
+    public Jogador jogador;
+
+	// Use this for initialization
+	void Start () {
+
+        campoTexto = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        campoTexto.text = "Vidas: " + jogador.GetVida();
+
+	}
+}
